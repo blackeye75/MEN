cbacf08d1947308c120e703a4164da5dc4f5fe0a
blackeye75 <priyanshuraj22275@gmail.com>
2024-12-18 19:52:48 +0530

    view engine set

--- index.js
+++ index.js
@@ -23,6 +23,8 @@
 const express = require("express")
 const app = express();
 
+app.set("view engine",'ejs')
+
 app.get('/', (req, res) => {
     res.send('Response form Home page');
 })
@@ -35,6 +37,6 @@ app.get('/profile', (req, res) => {
     res.send("Response from profile page");
 })
 
-app.listen(3000, () => {
+app.listen(3000, (port) => {
     console.log("Server is listening on port 3000");
 })
