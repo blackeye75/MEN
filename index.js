0855ec61dcc55d1000831a4df0013a50c6f9ce16
blackeye75 <priyanshuraj22275@gmail.com>
2024-12-19 13:38:33 +0530

    #

--- index.js
+++ index.js
@@ -23,7 +23,19 @@
 const express = require("express")
 const app = express();
 
-app.set("view engine",'ejs')
+app.set("view engine", 'ejs')
+
+app.use((req, res, next) => {
+    console.log("This is middleware");
+
+    const a = 5;
+    const b = 5;
+    const c = a + b;
+    console.log(c);
+    return next();
+
+})
+
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -37,6 +49,6 @@ app.get('/profile', (req, res) => {
     res.send("Response from profile page");
 })
 
-app.listen(3000, (port) => {
+app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 })
